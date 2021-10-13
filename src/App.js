5a5fb89e93465e326a35ece52f8b088d8042859a
Sergey <rodriges1989@mail.ru>
2021-10-13 11:41:56 +0200

    learn Hooks like useState()

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, {useState} from 'react';
-    // import React, { useEffect, useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+
 
 function App() {
-  const [likes, setLikes] = useState (5)
+
     const [value, setValue] = useState ('Text in input')
 
 
   return (
       <div className="App">
-         <h1>{likes}</h1>
-          <h1>{value}</h1>
-          <input
-              type="text"
-              value={value}
-              onChange={event => setValue(event.target.value)}
+   <ClassCounter/>
 
-          ></input>
 
     </div>
   );
